298e6c6b8971e4043b125e2161f776da92a704a5
GerardoBZentenoM <brallanzenteno@gmail.com>
2024-04-13 22:43:28 -0600

    added imstagram url

--- lib/TourScreen/TourScreen.dart
+++ lib/TourScreen/TourScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../QuizScreen/quiz_screen.dart';
 
@@ -10,12 +11,28 @@ class TourScreen extends StatelessWidget {
       body: Column(children: [
         Text("AR IS COOL"),
         ElevatedButton(
+          onPressed: () async {
+            var url =
+                'https://www.instagram.com/ar/2173879186288909/?ch=ZDQ1OWI1ZjBlZDAzNDIwZTlhOWU3ODI2MDc1MWVhNjQ%3D';
+
+            if (await canLaunch(url)) {
+              await launch(
+                url,
+                universalLinksOnly: true,
+              );
+            } else {
+              throw 'There was a problem to open the url: $url';
+            }
+          },
+          child: Text("Let's start a Game!"),
+        ),
+        ElevatedButton(
           onPressed: () {
             Navigator.of(context).push(
               MaterialPageRoute(builder: (context) => QuizScreen()),
             );
           },
-          child: Text("Let's start a Game!"),
+          child: Text("Let's see the Tour"),
         ),
       ]
           // QuizScreen
