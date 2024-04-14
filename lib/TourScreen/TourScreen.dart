6bd32091b26e2be96016c6243475d94614d5cb71
GerardoBZentenoM <brallanzenteno@gmail.com>
2024-04-14 00:25:27 -0600

    added ig lauched to AR

--- lib/TourScreen/TourScreen.dart
+++ lib/TourScreen/TourScreen.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../QuizScreen/quiz_screen.dart';
 
+Future<void> _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'No se pudo lanzar $url';
+  }
+}
+
 class TourScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,19 +18,27 @@ class TourScreen extends StatelessWidget {
       body: Column(children: [
         Text("AR IS COOL"),
         ElevatedButton(
-          onPressed: () async {
-            var url =
-                'https://www.instagram.com/ar/2173879186288909/?ch=ZDQ1OWI1ZjBlZDAzNDIwZTlhOWU3ODI2MDc1MWVhNjQ%3D';
+          onPressed: () =>
+              // _launchURL(
+              //     'https://www.instagram.com/ar/2173879186288909/?ch=ZDQ1OWI1ZjBlZDAzNDIwZTlhOWU3ODI2MDc1MWVhNjQ%3D'),
+              launchUrl(
+            Uri.parse(
+                'https://www.instagram.com/ar/2173879186288909/?ch=ZDQ1OWI1ZjBlZDAzNDIwZTlhOWU3ODI2MDc1MWVhNjQ%3D'),
+            mode: LaunchMode.externalApplication,
+          ),
+          // onPressed: () async {
+          //   var url =
+          //       'https://www.instagram.com/ar/2173879186288909/?ch=ZDQ1OWI1ZjBlZDAzNDIwZTlhOWU3ODI2MDc1MWVhNjQ%3D';
 
-            if (await canLaunch(url)) {
-              await launch(
-                url,
-                universalLinksOnly: true,
-              );
-            } else {
-              throw 'There was a problem to open the url: $url';
-            }
-          },
+          //   if (await canLaunch(url)) {
+          //     await launch(
+          //       url,
+          //       universalLinksOnly: true,
+          //     );
+          //   } else {
+          //     throw 'There was a problem to open the url: $url';
+          //   }
+          // },
           child: Text("Let's start a Game!"),
         ),
         ElevatedButton(
